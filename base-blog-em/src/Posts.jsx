325635611d1497f319d508da6965b53d91a2970a
Abby Humphreys <bibby3316@gmail.com>
2023-11-04 17:43:16 +0000

    initial commit

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from 'react-query';
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -14,8 +15,18 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  // useQuery takes 2 params the name of the query 
+  // and where the data is coming from
+  // must use async await
+  const {data, isError, error, isLoading} = useQuery('posts', fetchPosts, { staleTime: 2000, });
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError) 
+    return (
+        <>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+        </>
+      )
 
   return (
     <>
